f43a5a53448e6ea52b737738b50d6219fbdf88fb
Eyassu Getachew <eyassu@crazy-factory.com>
2016-12-21 03:57:58 +0700

    Added implementation and test cases for IMappingAlgorithmFactory

---
+++ src/TTF.Services.Tests/MappingAlgorithmFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TTF.Services.Algorithms;
+
+namespace TTF.Services.Tests
+{
+    [TestClass]
+    public class MappingAlgorithmFactoryTests
+    {
+        
+        [TestMethod]
+        public void BaseMapping_ShouldReturnBaseMappingAlgorithm()
+        {
+            var factory = new MappingAlgorithmFactory();
+            var algorithm = factory.GetMappingAlgorithm(MappingAlgorithmTypes.Base);
+            Assert.IsNotNull(algorithm);
+            Assert.AreEqual(typeof(BaseMapping), algorithm.GetType());
+        }
+
+        
+        [TestMethod]
+        public void Specialized1_ShouldReturnSpecialized1MappingAlgorithm()
+        {
+            var factory = new MappingAlgorithmFactory();
+            var algorithm = factory.GetMappingAlgorithm(MappingAlgorithmTypes.Specialized1);
+            Assert.IsNotNull(algorithm);
+            Assert.AreEqual(typeof(Specialized1), algorithm.GetType());
+        }
+
+        [TestMethod]
+        public void Specialized2_ShouldReturnSpecialized2MappingAlgorithm()
+        {
+            var factory = new MappingAlgorithmFactory();
+            var algorithm = factory.GetMappingAlgorithm(MappingAlgorithmTypes.Specialized2);
+            Assert.IsNotNull(algorithm);
+            Assert.AreEqual(typeof(Specialized2), algorithm.GetType());
+        }
+    }
+}
---
+++ src/TTF.Services/MappingAlgorithmFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TTF.Services
+{
+    public class MappingAlgorithmFactory : IMappingAlgorithmFactory
+    {
+        public IMappingAlgorithm GetMappingAlgorithm(MappingAlgorithmTypes type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
