490d64ba3d09d25be854b7bc0c8ef37997dc56fe
Eyassu Getachew <eyassug@gmail.com>
2016-12-21 15:51:18 +0700

    implemented Specialized2 overrides and extensions

--- src/TTF.Services.Tests/Algorithms/Specialized2Tests.cs
+++ src/TTF.Services.Tests/Algorithms/Specialized2Tests.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TTF.Services.Models;
+using TTF.Services.Algorithms;
 
 namespace TTF.Services.Tests.Algorithms
 {
@@ -6,8 +8,38 @@ namespace TTF.Services.Tests.Algorithms
     public class Specialized2Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ComputeX_ShouldReturnCorrectXValues()
         {
+            // A && B && !C => X.T
+            var actual1 = Specialized2.ComputeX(true, true, false);
+            var expected1 = X.T;
+            // A && !B && C => X.S
+            var actual2 = Specialized2.ComputeX(true, false, true);
+            var expected2 = X.S;
+
+            Assert.AreEqual(expected1, actual1);
+            Assert.AreEqual(expected2, actual2);
+
+        }
+
+        [TestMethod]
+        public void ComputeY_ShouldReturnCorrectYValues()
+        {
+            // X = S => Y = F + D + (D * E / 100)
+            var actualYs = Specialized2.ComputeY(X.S, 2, 3, 4);
+            // (X = R || X = T) => BaseMapping.ComputeY();
+            var actualYr = Specialized2.ComputeY(X.R, 2, 3, 4);
+            var actualYt = Specialized2.ComputeY(X.T, 2, 3, 4);
+
+            // expectedYs = 4 + 2 + (2*3/100) = 6 + (.06) = 6.06
+            var expectedYs = 6.06;
+            var expectedYr = BaseMapping.ComputeY(X.R, 2, 3, 4);
+            var expectedYt = BaseMapping.ComputeY(X.T, 2, 3, 4);
+
+
+            Assert.AreEqual(expectedYr, actualYr);
+            Assert.AreEqual(expectedYs, actualYs);
+            Assert.AreEqual(expectedYt, actualYt);
         }
     }
 }
--- src/TTF.Services/Algorithms/Specialized2.cs
+++ src/TTF.Services/Algorithms/Specialized2.cs
@@ -5,24 +5,20 @@ namespace TTF.Services.Algorithms
 {
     public class Specialized2 : BaseMapping
     {
-        protected override X ComputeX(IInputX input)
-        {
-            return base.ComputeX(input);
-        }
-
-        protected override double ComputeY(X x, IInputY input)
-        {
-            return base.ComputeY(x, input);
-        }
-
         public static new X ComputeX(bool a, bool b, bool c)
         {
-            throw new NotImplementedException();
+            if (a && b && !c)
+                return X.T;
+            if (a && !b && c)
+                return X.S;
+            return BaseMapping.ComputeX(a, b, c);
         }
 
         public static new double ComputeY(X x, int d, int e, int f)
         {
-            throw new NotImplementedException();
+            if (x == X.S)
+                return f + d + ((double)d * e / 100); // Also F + BaseMapping.ComputeY(x, d, e, f)
+            return BaseMapping.ComputeY(x, d, e, f);
         }
     }
 }
